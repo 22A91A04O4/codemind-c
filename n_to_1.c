d75bd9273a2d2369bff1f16ab944a580fe5b5266
22A91A04O4 <119720237+22A91A04O4@users.noreply.github.com>
2022-12-26 22:23:28 +0530

    #include<stdio.h> int main() {     int a,i;     scanf("%d",&a);     for(i=a;i>=1;i--)     {        printf("%d ",i);     } }

--- n_to_1.c
+++ n_to_1.c
@@ -1,11 +1,10 @@
 #include<stdio.h>
-int main ()
+int main()
 {
     int a,i;
     scanf("%d",&a);
     for(i=a;i>=1;i--)
     {
-        printf("%d ",i);
+       printf("%d ",i);
     }
-  return 0;
 }
