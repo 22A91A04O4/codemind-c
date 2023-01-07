230b81ab94b6fb4a8d78ef08bf2e9c6724193c46
22A91A04O4 <119720237+22A91A04O4@users.noreply.github.com>
2023-01-07 18:03:10 +0530

    #include<stdio.h> int main() {     int n,i,j,plus,minus,count;     scanf("%d",&n);     for(i=n;i>0;i++)     {         count=0;         for(j=1;j<=i;j++)         {             if(i%j==0)             count++;         }         if(count==2)         {             plus=i;             break;         }     }     if(plus==n)     printf("0");     else     {         for(i=n-1;i>0;i--)         {             count=0;             for(j=1;j<=i;j++)             {                 if(i%j==0)                 count++;             }             if(count==2)             {                 minus=i;                 break;             }         }         if(plus-n>n-minus)         printf("%d",n-minus);         else if(plus-n<n-minus)         printf("%d",plus-n);         else if(plus-n==n-minus)         printf("%d",n-minus);     } }

---
+++ Minimum_absolute_difference_of_a_number_and_its_closest_prime.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,j,plus,minus,count;
+    scanf("%d",&n);
+    for(i=n;i>0;i++)
+    {
+        count=0;
+        for(j=1;j<=i;j++)
+        {
+            if(i%j==0)
+            count++;
+        }
+        if(count==2)
+        {
+            plus=i;
+            break;
+        }
+    }
+    if(plus==n)
+    printf("0");
+    else
+    {
+        for(i=n-1;i>0;i--)
+        {
+            count=0;
+            for(j=1;j<=i;j++)
+            {
+                if(i%j==0)
+                count++;
+            }
+            if(count==2)
+            {
+                minus=i;
+                break;
+            }
+        }
+        if(plus-n>n-minus)
+        printf("%d",n-minus);
+        else if(plus-n<n-minus)
+        printf("%d",plus-n);
+        else if(plus-n==n-minus)
+        printf("%d",n-minus);
+    }
+}
