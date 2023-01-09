6b85c9d6a149870a538be2b7d63b0f099065000a
22A91A04O4 <119720237+22A91A04O4@users.noreply.github.com>
2023-01-09 21:22:27 +0530

    #include<stdio.h> int main() {     int a,b,i,n,c,s=0;     scanf("%d",&n);     int arr[100];     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     scanf("%d%d",&a,&b);     for(i=0;i<n;i++)     {         if(arr[i]<a||arr[i]>b)         {             s+=arr[i];         }     }     printf("%d",s); }

---
+++ sum_of_elements_not_between_A_and_B.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,i,n,c,s=0;
+    scanf("%d",&n);
+    int arr[100];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    scanf("%d%d",&a,&b);
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]<a||arr[i]>b)
+        {
+            s+=arr[i];
+        }
+    }
+    printf("%d",s);
+}
