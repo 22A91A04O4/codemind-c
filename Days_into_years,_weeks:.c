aa7faf7da874082b0c76fe499aaa35cf0382872a
22A91A04O4 <119720237+22A91A04O4@users.noreply.github.com>
2023-01-24 19:50:10 +0530

    #include<stdio.h> int main() {     int d,w,y;     scanf("%d",&d);     w=d/365;     y=(d%365)/7;     printf("%d %d",w,y);
    }

--- Days_into_years,_weeks:.c
+++ Days_into_years,_weeks:.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
 int main()
 {
-    int d,y,w;
+    int d,w,y;
     scanf("%d",&d);
-    y=d/365;
-    w=(d%365)/7;
+    w=d/365;
+    y=(d%365)/7;
     printf("%d
-%d",y,w);
+%d",w,y);
+    
 }
